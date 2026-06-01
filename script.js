f07c9e97c57d8c04edc30163d0f51a5035983a2e
Gonzalo De La Fuente <gonzalodelafuente1313@gmail.com>
2026-06-01 19:55:20 -0300

    se creo el script de la pagina

---
+++ script.js
@@ -0,0 +1,22 @@
+const meriendas = [
+    { nombre: 'Merienda de Chocolate', precio: '$10' },
+    { nombre: 'Merienda de Frutas', precio: '$8' },
+    { nombre: 'Merienda de Queso y Jamón', precio: '$12' },
+    { nombre: 'Merienda de Patatas Fritas', precio: '$9' },
+    { nombre: 'Merienda de Yogur con Frutas', precio: '$11' }
+];
+
+const meriendasContainer = document.getElementById('menu-meriendas');
+const htmlMeriendas = meriendas.map((merienda) => `
+    <div class="merienda">
+        <h3>${merienda.nombre}</h3>
+        <p>Precio: ${merienda.precio}</p>
+    </div>
+`).join('');
+meriendasContainer.innerHTML = htmlMeriendas;
+
+const mostrarTamañoArregloButton = document.getElementById('mostrar-tamaño-arreglo');
+mostrarTamañoArregloButton.addEventListener('click', () => {
+    const tamañoArreglo = meriendas.length;
+    alert(`El arreglo tiene ${tamañoArreglo} elementos.`);
+});
